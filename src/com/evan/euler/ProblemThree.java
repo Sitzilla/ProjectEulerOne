a87fededcee7c2854c5788657200c432316049fe
Evan Sitzes <esitzes42@hotmail.com>
2014-05-30 17:21:33 -0500

    worked on Euler problem three

--- src/com/evan/euler/ProblemThree.java
+++ src/com/evan/euler/ProblemThree.java
@@ -3,28 +3,45 @@ package com.evan.euler;
 public class ProblemThree {
 
 	public static void main(String args[]) {
-		int answer = 0;
+		long answer = 0;
 		
 		answer = ProblemThree();
 		
 		System.out.println(answer);
 	}
 	
-	public static int ProblemThree(){
+	public static long ProblemThree(){
+		boolean primeNumber = false;
+		int largestPrime = 0;
+		long l = 0;
 		
-		for (long l=60085147514;i<0;i--) {
+		for (l = (long) 2.0; l<=600851475143.0; l++) {
+				
+			if (600851475143.0%l==0){
+				long factor = (long) (600851475143.0/l);
+				primeNumber = isPrime(factor);
+			}
 			
-			
-			
+			if (primeNumber == true) {
+				return l;
+			}
+		
+		
+		}
+		return 0;
+	}
+	
+	public static boolean isPrime(long value){
+		
+		for (long i = (long) 2.0; i < value ;i++){
+			if (value%i == 0) {
+				return false;
+			}
 			
 		}
 		
 		
-		
-		
-		
+		return true;
 	}
-	
-	
 	
 }
